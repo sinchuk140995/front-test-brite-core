aa64bb6b7679d749a36b011f5fc06013896cac4b
Bohdan Sinchuk <sinchuk140995@gmail.com>
2018-11-07 00:12:28 +0200

    init

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div id="app">
+    <insurance-risks></insurance-risks>
+  </div>
+</template>
+
+<script>
+  import InsuranceRisks from './InsuranceRisks.vue'
+
+
+  export default {
+    name: 'app',
+    components: {
+      InsuranceRisks
+    },
+    data () {
+      return {
+      }
+    },
+  }
+</script>
+
+<style>
+</style>
---
+++ src/InsuranceRiskList.vue
@@ -0,0 +1,30 @@
+<template>
+  <ul>
+    <h1>{{name}}</h1>
+    <insurance-risk name="Insurance 1"></insurance-risk>
+  </ul>
+</template>
+
+<script>
+import InsuranceRisk from './InsuranceRisk.vue'
+
+
+export default {
+  name: 'insurance-risk-list',
+  components: {
+    InsuranceRisk
+  },
+  props: {
+    name: {
+      type: String
+    }
+  },
+  data () {
+    return {
+    }
+  },
+}
+</script>
+
+<style>
+</style>
