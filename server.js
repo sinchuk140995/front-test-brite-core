40cd7fa67bdda79590e90029205a5f9159f25de2
Bohdan Sinchuk <sinchuk140995@gmail.com>
2018-11-21 01:03:13 +0200

    server

--- server.js
+++ server.js
@@ -1,7 +1,11 @@
-const express = require('express');
-const serveStatic = require("serve-static")
-const path = require('path');
-app = express();
-app.use(serveStatic(path.join(__dirname, 'dist')));
-const port = process.env.PORT || 80;
-app.listen(port);
+var static = require("node-static");
+var file = new static.Server(".", { serverInfo: "test-brite" });
+require("http")
+  .createServer(function(request, response) {
+    request
+      .addListener("end", function() {
+        file.serve(request, response);
+      })
+      .resume();
+  })
+  .listen(process.env.PORT || 3000);
